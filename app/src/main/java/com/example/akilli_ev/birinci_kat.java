a62d1e71092e3dc665bf634466f58c820f688818
iremyuce <iremyuce3452@gmail.com>
2024-05-19 11:58:21 +0300

    drawable

---
+++ app/src/main/java/com/example/akilli_ev/birinci_kat.java
@@ -0,0 +1,14 @@
+package com.example.akilli_ev;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class birinci_kat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_birinci_kat);
+    }
+}
