026fd5357c7cb4f4ae56b5e4c985691292c16187
iremyuce <iremyuce3452@gmail.com>
2024-05-19 12:21:57 +0300

    Main

---
+++ app/src/main/java/com/example/akilli_ev/CalismaOdasi.java
@@ -0,0 +1,14 @@
+package com.example.akilli_ev;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CalismaOdasi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calisma_odasi);
+    }
+}
